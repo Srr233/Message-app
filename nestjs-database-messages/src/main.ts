beadf6c0a3674c4538c177bb86732a09c31702e9
Sergey <Sr233@yandex.ru>
2024-06-01 13:49:30 +0300

    refactor

--- nestjs-database-messages/src/main.ts
+++ nestjs-database-messages/src/main.ts
@@ -8,9 +8,7 @@ async function bootstrap() {
   const configService = app.get<ConfigService>(ConfigService);
   app.setGlobalPrefix('api');
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
-  app.enableCors({
-    origin: configService.get('ORIGIN'),
-  });
+  app.enableCors({ origin: configService.get('ORIGIN') });
   await app.listen(configService.get('REACT_APP_BACKEND_PORT') || 4000);
 }
 bootstrap();
